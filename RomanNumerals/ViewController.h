2be3bd59c1fe3106e0dadf08db80ebb00b3f99b8
Jeremy Ong <jeremy.ongws@gmail.com>
2016-03-30 17:39:17 +0800

    finish

--- RomanNumerals/ViewController.h
+++ RomanNumerals/ViewController.h
@@ -13,3 +13,67 @@
 
 @end
 
+//begin
+
+//- (NSString *)convertToRoman:(NSString *)number {
+//	NSString *romanNumerals = [[NSString alloc] init];
+//
+//	for (NSUInteger lengthOfNum = [number length]; lengthOfNum > 0 ; lengthOfNum--) {
+//		NSUInteger digit = [[number substringToIndex:1] intValue];
+//		if (lengthOfNum == 4){
+//			for (; digit > 0; digit--) {
+//				if (digit == 9){
+//					romanNumerals = [romanNumerals stringByAppendingString:@"IM"];
+//				} else {
+//					romanNumerals = [romanNumerals stringByAppendingString:@"M"];
+//				}
+//			}
+//		} else if (lengthOfNum == 3){
+//				NSUInteger remainder = digit % 5;
+//				if (digit >= 5) {
+//					if (digit == 4){
+//						romanNumerals = [romanNumerals stringByAppendingString:@"ID"];
+//					} else {
+//						romanNumerals = [romanNumerals stringByAppendingString:@"D"];
+//					}
+//				}
+//			  for (; remainder > 0; remainder--){
+//						romanNumerals = [romanNumerals stringByAppendingString:@"C"];
+//				}
+//		} else if (lengthOfNum == 2){
+//			NSUInteger remainder = digit % 5;
+//			if (digit >= 5) {
+//				if (digit == 4){
+//					romanNumerals = [romanNumerals stringByAppendingString:@"IL"];
+//
+//				} else {
+//					romanNumerals = [romanNumerals stringByAppendingString:@"L"];
+//				}
+//			}
+//			for (; remainder > 0; remainder--){
+//				romanNumerals = [romanNumerals stringByAppendingString:@"X"];
+//			}
+//		} else if (lengthOfNum == 1){
+//			NSUInteger remainder = digit % 5;
+//			if (digit >= 5) {
+//				if (digit == 4){
+//					romanNumerals = [romanNumerals stringByAppendingString:@"IV"];
+//					continue;
+//				} else if (digit == 9){
+//					romanNumerals = [romanNumerals stringByAppendingString:@"IX"];
+//					continue;
+//				}else {
+//					romanNumerals = [romanNumerals stringByAppendingString:@"V"];
+//					continue;
+//				}
+//			}
+//			for (; remainder > 0; remainder--){
+//				romanNumerals = [romanNumerals stringByAppendingString:@"I"];
+//			}
+//	  }
+//	}
+//	NSLog(@"%@", romanNumerals);
+//	return romanNumerals;
+//}
+
+// end
--- RomanNumerals/ViewController.m
+++ RomanNumerals/ViewController.m
@@ -9,6 +9,8 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *numberField;
+@property (weak, nonatomic) IBOutlet UILabel *convertedNumber;
 
 @end
 
@@ -16,12 +18,45 @@
 
 - (void)viewDidLoad {
 	[super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+	self.convertedNumber.text = @"";
 }
 
-- (void)didReceiveMemoryWarning {
-	[super didReceiveMemoryWarning];
-	// Dispose of any resources that can be recreated.
+- (IBAction)onNumeralifyButtonPressed:(id)sender {
+	NSString *input = self.numberField.text;
+	self.convertedNumber.text = [self convertToRoman:input];
 }
+
+
+- (NSString *)convertToRoman:(NSString *)number {
+	NSInteger numberInt = [number integerValue];
+	NSDictionary *keyValue = @{
+														 @"I": [NSNumber numberWithInt:1],
+														 @"IV": [NSNumber numberWithInt:4],
+														 @"V": [NSNumber numberWithInt:5],
+														 @"IX":[NSNumber numberWithInt:9],
+														 @"X": [NSNumber numberWithInt:10],
+														 @"IL":[NSNumber numberWithInt:40],
+														 @"L": [NSNumber numberWithInt:50],
+														 @"IC":[NSNumber numberWithInt:90],
+														 @"C": [NSNumber numberWithInt:100],
+														 @"ID": [NSNumber numberWithInt:400],
+														 @"D": [NSNumber numberWithInt:500],
+														 @"IM": [NSNumber numberWithInt:900],
+														 @"M": [NSNumber numberWithInt:1000]
+														 };
+	NSString *romanNumeral = [[NSString alloc] init];
+	
+	NSArray *array = @[@"M", @"IM", @"D", @"ID", @"C", @"IC", @"L", @"IL", @"X", @"IX", @"V", @"IV", @"I"];
+	for (id item in array){
+		NSInteger division = numberInt / [[keyValue objectForKey:item] integerValue];
+		NSInteger remainder = numberInt % [[keyValue objectForKey:item] integerValue];
+		for (; division > 0; division--) {
+			romanNumeral = [romanNumeral stringByAppendingString:item];
+		}
+		numberInt = remainder;
+	}
+	return romanNumeral;
+}
+
 
 @end
